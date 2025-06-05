bf17611c340d4b7c80192ebd37d34d9f53b5539c
François Nsengimana <francknsengimana@gmail.com>
2025-06-05 10:08:46 +0100

    mise en place de l'environement du projet creer avec le startspring.io

---
+++ src/test/java/com/imana47/flowatch/FlowatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imana47.flowatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
